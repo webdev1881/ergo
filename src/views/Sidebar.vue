054f70b7531d0e57fa12de9fd4d65f63a39364da
Литвиновський Володимир Володимирович <Litvinovskiy-V@MC.GCF>
2019-11-27 17:26:29 +0200

    xcv

--- src/views/Sidebar.vue
+++ src/views/Sidebar.vue
@@ -1,38 +1,46 @@
 <template>
   <div class="sidebar">
-      <router-link     
-        v-for="link in links"
-        :key="link.url"
-        tag="div"
-        class="item"
-        active-class="active"
-        :to="link.url"
-        :exact="link.exact"
-      >
-        <i class="material-icons">{{link.icon}}</i>
-        <div class="title">{{link.title}}</div>
-      </router-link>
+    <a href="#" @click.prevent="$emit('')">
+      <i class="lines material-icons black-text">dehaze</i>
+    </a>
+    <router-link
+      v-for="link in links"
+      :key="link.url"
+      tag="div"
+      class="item"
+      active-class="active"
+      :to="link.url"
+      :exact="link.exact"
+    >
+      <i class="material-icons">{{link.icon}}</i>
+      <div class="title">{{link.title}}</div>
+    </router-link>
   </div>
 </template>
 
 
 <script>
 export default {
-  props: ['value'],
+  props: ["value"],
   data: () => ({
     links: [
-      {title: 'Счет', icon: 'cloud', url: '/', exact: true},
-      {title: 'История', icon: 'cloud', url: '/history'},
-      {title: 'Новая запись', icon: 'cloud', url: '/record'},
-      {title: 'Категории', icon: 'cloud', url: '/categories'}
+      { title: "Счет", icon: "cloud", url: "/", exact: true },
+      { title: "История", icon: "cloud", url: "/history" },
+      { title: "Новая запись", icon: "cloud", url: "/record" },
+      { title: "Категории", icon: "cloud", url: "/categories" }
     ]
-  }),
-
-
-}
+  })
+};
 </script>
 
 <style lang="scss" >
+a {
+  color: black;
+  .lines {
+    font-size: 30px;
+    margin-left: 7px;
+  }
+}
 
 .sidebar {
   border-right: 1px solid gray;
@@ -42,11 +50,12 @@ export default {
 .item {
   width: 100%;
   display: flex;
+  // flex-direction: row-reverse;
   align-items: center;
   padding: 10px 0px 10px 0px;
-  transition: all .2s;
+  transition: all 0.2s;
   &:hover {
-    background-color: #FFF;
+    background-color: #fff;
     cursor: pointer;
   }
 }
@@ -56,11 +65,9 @@ export default {
   color: #094a69;
   margin-left: 10px;
   text-decoration: none;
-  
 }
 i {
   margin-left: 10px;
+  margin-right: 10px;
 }
-
-
 </style>
