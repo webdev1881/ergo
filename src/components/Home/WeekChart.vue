3ecef0d42cb9f113b8c75884417818129fcea65c
webdev1881 <webdev1881@gmail.com>
2020-09-25 19:43:52 +0300

    456

--- src/components/Home/WeekChart.vue
+++ src/components/Home/WeekChart.vue
@@ -4,15 +4,21 @@
       <Loader />
     </div>
 
-    <canvas class="canvas" ref="canvas"></canvas>
+    <canvas class="canvas" @ ref="canvas">
+      
+    </canvas>
 
-    <button  class="waves-effect waves-light btn" @click="add" >654654645</button>
+    <button v-if="!isLoading" class="waves-effect waves-light btn-small" @click="add">Добавить</button>
+    <button v-if="!isLoading" class="waves-effect waves-light btn-small" @click="remove">Удалить</button>
+
+
 
   </div>
 </template>
 
 <script>
 import { Line } from "vue-chartjs";
+
 
 export default {
   name: "WeekChart",
@@ -49,18 +55,37 @@ export default {
   },
 
   methods: {
+
     add() {
-        // let firstWeek = +this.weeks[0].week
-        // this.weeks.push(this.weeksValue[this.weeksValue.length - +firstWeek])
-        // console.log( this.weeks);
+      let firstWeek = this.weeks.length;
+      let newData = (this.weeksValue[this.weeksValue.length - 1 - firstWeek])
+      if (this.weeks.length < this.weeksValue.length) {  
+        this.weeks.unshift(newData);
+        this.$data._chart.data.labels.unshift(newData.week);
+        this.$data._chart.data.datasets[0].data.unshift(newData.units);
+        this.$data._chart.update()     
+      }
     },
+
+    remove() {
+      let firstWeek = this.weeks.length;
+      let newData = (this.weeksValue[this.weeksValue.length - firstWeek])
+      if (this.weeks.length <= this.weeksValue.length && this.weeks.length > 2) {       
+        this.weeks.shift(newData);
+        this.$data._chart.data.labels.shift(newData.week);
+        this.$data._chart.data.datasets[0].data.shift(newData.units);
+        this.$data._chart.update()     
+      }
+    },
+
     render() {
       for (let i = this.num; i > 0; i--) {
         this.weeks.push(this.weeksValue[this.weeksValue.length - i])
       }
+      // console.log(this.weeks);
       this.renderChart(
         {
-          labels: this.weeks.map((w) => w.week),
+          labels: this.weeks.map((w) => w.week) || null,
           datasets: [
             {
               label: "GFK Units",
@@ -70,10 +95,10 @@ export default {
               data: this.weeks.map((w) => w.units),
             },
           ],
-        },
-        this.options
-      );
+        }, this.options);
     },
+
+
   },
 };
 </script>
@@ -81,5 +106,8 @@ export default {
 <style lang="scss" >
 .week {
   width: 50%;
+}
+.btn-small {
+  margin-right: 20px;
 }
 </style>
